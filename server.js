d66f310908daa5e72e563a01269b384cf816ce23
treeswithlove <monet.2017@gmail.com>
2019-04-24 20:50:33 -0400

    connected files and created file structure

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+
+const userApi = require('./api/userApi.js');
+const accountApi = require('./api/waysApi.js');
+
+//sets up hbs
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+"/public"));
+
+//setup middleware for handling html forms
+//where body is a query string 
+app.use(express.urlencoded());
+
+
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.render("index/index")
+});
+
+
+
+//ALWAYS AT BOTTOM OF FILE
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("server has started");
+});
