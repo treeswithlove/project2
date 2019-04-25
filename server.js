83f3b1cbf4a581248e6bf03f2ee4ee13598a219a
treeswithlove <monet.2017@gmail.com>
2019-04-25 13:28:27 -0400

    updated server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/way', (req, res) => {
 
 
 //ALWAYS AT BOTTOM OF FILE
-const PORT = 3000;
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("server has started");
 });
