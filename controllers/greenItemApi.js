afe5cd2e30b1c35d3a888a3f2ed92cbc44ec4a47
treeswithlove <monet.2017@gmail.com>
2019-04-25 20:45:48 -0400

    updating how i organize files

---
+++ controllers/greenItemApi.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express();
+const GreenItemModel = require('../')
+
+router.get('/', (req, res) => {
+    res
+})
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 
-const userApi = require('./api/userApi.js');
-const accountApi = require('./api/waysApi.js');
+const userApi = require('./controllers/api/userApi.js');
+const locationApi = require('./models/locationApi.js/index.js');
+const greenItemApi = require('./controllers/api/greenItemApi.js');
 
 //sets up hbs
 app.set('view engine', 'hbs');
@@ -16,18 +17,22 @@ app.use(express.urlencoded());
 
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.render("index")
-});
-app.get('/user', (req, res) => {
-  res.render("user")
-});
-app.get('/ways', (req, res) => {
-  res.render("ways")
-});
-app.get('/way', (req, res) => {
-  res.render("way")
-});
+
+app.use('api/greenItem', greenItemApi)
+app.use('api/user', userApi)
+app.use('api/location', locationApi)
+// app.get('/', (req, res) => {
+//   res.render("index")
+// });
+// app.get('/user', (req, res) => {
+//   res.render("user")
+// });
+// app.get('/ways', (req, res) => {
+//   res.render("ways")
+// });
+// app.get('/way', (req, res) => {
+//   res.render("way")
+// });
 
 
 
