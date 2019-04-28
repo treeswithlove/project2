033034d0b9a2e56c9531e1bf85c948bffdbac2a0
treeswithlove <monet.2017@gmail.com>
2019-04-28 09:34:14 -0400

    edited file structure

---
+++ controllers/greenItem.js
@@ -0,0 +1,64 @@
+// //user model
+// const UserModel = require('../models/schema')
+
+
+// //user controller
+// const UserController = {
+//     //finds all users 
+//         index: function(req, res){
+//             UserModel.UserModel.find().then(user => {
+//                 res.render('user/index', {user})
+//                 // res.json(user)
+//             })
+//         },
+//     //goes to create new user route
+//         new: function(req, res){
+//             res.render('user/new')
+//         },
+//     //creates new user
+//         create: function(req,res){
+//             // UserModel.create(req.body)
+//             const newUser = (req.body)
+//             UserModel.UserModel.create(newUser)
+//             .then((user) => {
+//                 res.json(user)
+//             })
+//             // .then(() => res.redirect("/"))
+//         },
+//     //show one user by id
+//         show: function(req,res){
+//             UserModel.UserModel.findById(req.params.userId)
+//             // .then((user) => res.render('user/show', {
+//             //     user
+//             // })).catch(error => {
+//             //     console.log(error)
+//             // })
+//             .then((user) => {
+//                 res.json(user)
+//             })
+//         },
+//         // //find one user by id and edit user
+//         edit: function(req,res){
+//             UserModel.UserModel.findById(req.params.userId)
+//                 .then((user => res.render('user/edit', {user}))).catch(error => console.log(error))
+//         },
+//         // //update one user by id
+//         update: function(req,res){
+//             UserModel.UserModel.findByIdAndUpdate(req.params.userId, req.body).then(() => {
+//                 res.send()
+//             })
+//         },
+//         // //find user by id and remove user
+//         delete: function(req,res){
+//             UserModel.UserModel.findByIdAndDelete(req.params.userId).then((user) => {
+//                 res.json(user)
+//                 // res.redirect('/users').catch((error) => {
+//                 //     console.log(error)
+//                 // })
+//             })
+//         }
+     
+//     }
+
+
+// module.exports = UserController
--- routes/routes.js
+++ routes/routes.js
@@ -12,20 +12,20 @@ router.get('/:userId',userController.show)
 router.put('/:userId',userController.update)
 router.delete('/:userId', userController.delete)
 
-//greenItem controller routes
-router.get('/:userId', greenItemController.index)
-router.post('/:userId', greenItemController.create)
-router.get('/:userId/new', greenItemController.new)
-router.get('/:userId/:greenItemId',greenItemController.show)
-router.put('/:userId/:greenItemId',greenItemController.update)
-router.delete('/:userId/:greenItemId', greenItemController.delete)
+// //greenItem controller routes
+// router.get('/:userId', greenItemController.index)
+// router.post('/:userId', greenItemController.create)
+// router.get('/:userId/new', greenItemController.new)
+// router.get('/:userId/:greenItemId',greenItemController.show)
+// router.put('/:userId/:greenItemId',greenItemController.update)
+// router.delete('/:userId/:greenItemId', greenItemController.delete)
 
-//location controller routes
-router.get('/:userId/:greenItemId', locationController.index)
-router.post('/:userId/:greenItemId', locationController.create)
-router.get('/:userId/:greenItemId/new', locationController.new)
-router.get('/:userId/:greenItemId/:locationId',locationController.show)
-router.put('/:userId/:greenItemId/:locationId',locationController.update)
-router.delete('/:userId/:greenItemId/:locationId', locationController.delete)
+// //location controller routes
+// router.get('/:userId/:greenItemId', locationController.index)
+// router.post('/:userId/:greenItemId', locationController.create)
+// router.get('/:userId/:greenItemId/new', locationController.new)
+// router.get('/:userId/:greenItemId/:locationId',locationController.show)
+// router.put('/:userId/:greenItemId/:locationId',locationController.update)
+// router.delete('/:userId/:greenItemId/:locationId', locationController.delete)
 
 module.exports = router
